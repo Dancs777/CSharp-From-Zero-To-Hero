1a7504bca50dc6a90651f54d38af8dcdbeab819d
Dancs777 <59937218+Dancs777@users.noreply.github.com>
2020-02-06 11:51:10 +0100

    arrayic testing, nothing important

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -4,6 +4,29 @@
     {
         static void Main(string[] args)
         {
+            CheckAndResetWindowSize();
+            int[] numbers = new int[10];
+            numbers[0] = 2;
+            numbers[1] = 4;
+            numbers[2] = 8;
+            numbers[3] = 16;
+            numbers[4] = 32;
+            numbers[5] = 64;
+            numbers[6] = 128;
+            numbers[7] = 256;
+            numbers[8] = 512;
+            numbers[9] = 1048;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        private static void CheckAndResetWindowSize()
+        {
+            if (Console.WindowHeight != 300 || Console.WindowWidth != 500)
+            {
+                Console.SetWindowSize(120, 40);
+            }
         }
     }
 }
